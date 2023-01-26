3292d840c81fe7573d85caaecf307102afb9a074
Demigod <austinjobs2shop@gmail.com>
2023-01-26 10:35:35 -0800

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,6 +1,6 @@
 local focus = false
 
-RegisterCommand("openui", function()
+RegisterCommand("toggleui", function()
     focus = not focus
     SetNuiFocus(focus, focus) --enables mouse and keyboard controls for the nui / disables game control
     SendNUIMessage({ type = 'openUI', toggle = focus }) --sends information from lua -> React (see handleMessage in app.tsx)
